086fd5b50272348d4c6b137e4bdd496b1a3bd13c
Marcelo <marceezemamani@gmail.com>
2024-08-16 16:19:41 -0300

    inportando frameworks kivy

---
+++ app_biblio.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.dropdown import DropDown
+from kivy.clock import Clock
